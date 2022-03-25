68fea487899c426c9811584dc2686b6d84969090
JohnKeng <m0920173456@gmail.com>
2022-03-26 02:19:10 +0800

    add fn-add sample

--- curry.js
+++ curry.js
@@ -1,30 +1,41 @@
 function curry(func , fixedParams){
   if ( !Array.isArray(fixedParams) ) { fixedParams = [ ] }
   return function(){
-      let newParams = Array.prototype.slice.call(arguments); 
+      let newParams = Array.prototype.slice.call(arguments);
       if ( (fixedParams.length+newParams.length) < func.length ) {
-          return curry(func , fixedParams.concat(newParams));
+          return curry(func , fixedParams.concat(newParams))
       }else{
-          return func.apply(undefined, fixedParams.concat(newParams));
+          return func.apply(undefined, fixedParams.concat(newParams))
       }
-  };
+  }
 }
 
 
-var abc = function(a, b, c) {
-  return [a, b, c];
-};
+const abc = function(a, b, c) {
+  return [a, b, c]
+}
 
-var curried = curry(abc);
+const curried = curry(abc)
 
-curried(1)(2)(3);
+curried(1)(2)(3)
 // => [1, 2, 3]
 
-curried(1, 2)(3);
+curried(1, 2)(3)
 // => [1, 2, 3]
 
-curried(1, 2, 3);
+curried(1, 2, 3)
 // => [1, 2, 3]
 
 curried(1)(2,3);
 // => [1, 2, 3]
+
+
+
+function sum(a, b, c) {
+    return a + b + c
+}
+
+const curriedSum = curry(sum)
+
+curriedSum(1)(2)(3)
+// 6
